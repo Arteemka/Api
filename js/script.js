d11210648124ad56efaa4edb6017cdc5c6e3c566
Artemka <artemiy.rak@mail.ru>
2019-11-12 09:32:05 +0300

    get  date with server via promise

--- js/script.js
+++ js/script.js
@@ -1,34 +1,24 @@
-let findInput = document.getElementsByClassName('find-item-text')[0];
-const buttonFind = document.getElementsByClassName('find-item-button')[0];
+let findInput = document.getElementsByClassName("find-item-text")[0];
+const buttonFind = document.getElementsByClassName("find-item-button")[0];
 
-buttonFind.addEventListener('click',createScript);
+// async function getRes(){
+//     let response = await fetch(`https://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=uk&listing_type=rent&page=1&place_name=${findInput.value}`);
+//     let content = await response.json();
+//     console.log(content.response.listings);
+// }
 
-function createScript() {
-    const script = document.createElement('script');
-    const url = constructQueryParams(findInput.value);
-    script.type = 'text/javascript';    
-    script.src = url;
-    document.body.appendChild(script);
-    script.parentNode.removeChild(script);
-}
 
-function constructQueryParams(searchInput, page = 1) {
-    const url = 'https://api.nestoria.co.uk/api?';
-    let params = new URLSearchParams();
-
-    params.append('encoding', 'json');
-    params.append('pretty', '1');
-    params.append('action', 'search_listings');
-    params.append('country', 'uk');
-    params.append('listing_type', 'rent');
-    params.append('page', `${page}`);
-    params.append('place_name', searchInput);
-    params.append('callback', 'date');
   
-    return url + params;
-}
-
-function date (obj) {
-console.log(obj.response.listings);
-}
+buttonFind.addEventListener('click',function (){
+    fetch(`https://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=uk&listing_type=rent&page=1&place_name=${findInput.value}`)  
+    .then(function(response) {  
+      return response.json();  
+    })  
+    .then(function(date) {  
+      console.log('Request successful', date.response.listings);  
+    })  
+    .catch(function(error) {  
+      log('Request failed', error)  
+    });
+})
 
