d0dc586988686c02d312a77efe5411ecbecff22f
Artemka <artemiy.rak@mail.ru>
2019-11-12 09:43:16 +0300

    fix format

--- js/script.js
+++ js/script.js
@@ -1,24 +1,17 @@
 let findInput = document.getElementsByClassName("find-item-text")[0];
 const buttonFind = document.getElementsByClassName("find-item-button")[0];
 
-// async function getRes(){
-//     let response = await fetch(`https://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=uk&listing_type=rent&page=1&place_name=${findInput.value}`);
-//     let content = await response.json();
-//     console.log(content.response.listings);
-// }
-
-
-  
-buttonFind.addEventListener('click',function (){
-    fetch(`https://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=uk&listing_type=rent&page=1&place_name=${findInput.value}`)  
-    .then(function(response) {  
-      return response.json();  
-    })  
-    .then(function(date) {  
-      console.log('Request successful', date.response.listings);  
-    })  
-    .catch(function(error) {  
-      log('Request failed', error)  
+buttonFind.addEventListener("click", function() {
+  fetch(
+    `https://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=uk&listing_type=rent&page=1&place_name=${findInput.value}`
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(date) {
+      console.log("Request successful", date.response.listings);
+    })
+    .catch(function(error) {
+      console.log("Request failed", error);
     });
-})
-
+});
